d5427ddd1e6e85aff7a401d12d64e88135f70fc9
Jossi Wolf <jossi@snappmobile.io>
2021-03-01 13:56:19 +0100

    Studio composable reference from non-composable context error message

---
+++ sample-compose/src/main/java/com/airbnb/lottie/sample/compose/StudioErrorMessageRepro.kt
@@ -0,0 +1,12 @@
+package com.airbnb.lottie.sample.compose
+
+import androidx.compose.runtime.Composable
+
+fun StudioErrorMessageRepro() {
+    SimpleComposable()
+}
+
+@Composable
+private fun SimpleComposable() {
+
+}
